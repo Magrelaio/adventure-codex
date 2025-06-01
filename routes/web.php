458658ae0c4bf3316a5b57fc32ad3dd485438309
Magrelaio <caiocesar2004gfgff@gmail.com>
2025-06-01 04:30:44 -0300

    fix: Configurando o projeto corretamente para implementação de TailwindCSS e Laravel Broadcasting

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', function () {
+//     return redirect()->route('login');
+// });
+
+Route::get('/', function(){
+    return view('index');
 });
